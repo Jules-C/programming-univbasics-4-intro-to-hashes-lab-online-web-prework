3174a961575c57f4e20e21b106e035cdeae28b7c
Julie Cook <juliecook328@gmail.com>
2019-07-11 01:41:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,9 +41,9 @@ def update_counting_hash(hash, key)
   hash = {}
   
   if hash[key]
-  hash[key]  = hash[key] += 1
+    hash[key]  = hash[key] += 1
   else
-  hash[key] = 1
+    hash[key] = 1
   end
   hash 
 end 
